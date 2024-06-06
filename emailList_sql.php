afd362e4bf28ffe2256b4fb1040e9933a6ba30a0
Alex Iannuzzi <emilyiannuzzi8@gmail.com>
2024-06-06 10:33:26 -0500

    implemented  PHPMailer

--- emailList_sql.php
+++ emailList_sql.php
@@ -1,19 +1,58 @@
 <!--Sends email to user (confirming message) and me-->
 
 <?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+use PHPMailer\PHPMailer\SMTP;
+include dirname(__DIR__) .'/PHPMAILER/PHPMailer.php';
+include dirname(__DIR__) .'/PHPMAILER/Exception.php';
+include dirname(__DIR__) .'/PHPMAILER/SMTP.php';
 
-$message = $_POST['message'];
-$message = wordwrap($message, 70, "\r\n");
+function checkSend($mail){
+    if( !$mail->send() ){
+        $tab = array('error' => 'Mailer Error: '.$mail->ErrorInfo );
+        echo json_encode($tab);
+        return false;
+    }
+    else{
+        echo "<script>alert('Message sent successfully!')</script>";
+        return true;
+    }
+}
 
-$cust_email = $_POST['email'];
-$name = $_POST['name'];
-$sas_email = "sleipnirartstudio@gmail.com";
+$cmail = new PHPMailer();
+$cmail->CharSet = 'UTF-8';
+$cmail->STMPSecure = 'tls';
+$cmail->Host = 'smtp.gmail.com';
+$cmail->SMTPAuth = true;
+$cmail->Username = 'sleipnirartstudio@gmail.com';
+$cmail->Password = 'ewxd ttlh kwsr avcp';
+$cmail->Port = 587;
+$cmail->setFrom('sleipnirartstudio@gmail.com', 'Sleipnir Art Studio');
+$cmail->addAddress($_POST['email']);
+$cmail->addReplyTo('sleipnirartstudio@gmail.com', 'Sleipnir Art Studio');
+$cmail->isHTML(true);
+$cmail->Subject = 'Message Confirmation';
+$cmail->Body = "Thank you for your message, " . $_POST['name'] . "! We will get back to you as soon as possible.";
 
-$cust_header = "From: $sas_email";
-mail($cust_email, "Message Confirmation", "Thank you for your message, $name! We will get back to you as soon as possible.", $cust_header);
-mail($sas_email, "New Message", "You have a new message from $name. Here is the message: $message", "From: $cust_email");
+$smail = new PHPMailer();
+$smail->CharSet = 'UTF-8';
+$smail->STMPSecure = 'tls';
+$smail->Host = 'smtp.gmail.com';
+$smail->SMTPAuth = true;
+$smail->Username = 'sleipnirartstudio@gmail.com';
+$smail->Password = 'ewxd ttlh kwsr avcp';
+$smail->Port = 587;
+$smail->setFrom('sleipnirartstudio@gmail.com', 'Sleipnir Art Studio');
+$smail->addAddress('sleipnirartstudio@gmail.com');
+$smail->addReplyTo('sleipnirartstudio@gmail.com', 'Sleipnir Art Studio');
+$smail->isHTML(true);
+$smail->Subject = 'New Message from ' . $_POST['name'];
+$smail->Body = "You have a new message from " . $_POST['name'] . ". Here is the message: " . $_POST['message'];
 
-echo "<script>alert('Message sent successfully!')</script>";
+checkSend($cmail);
+checkSend($smail);
+
 echo"<script>window.location.href='contact.php'</script>";
 
 ?>
